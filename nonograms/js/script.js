582671e1a967ed910e6776663f6f62b0b2aa7972
natagetup <n.kulikouskaya@gmail.com>
2024-01-22 22:29:04 +0100

    feat: implement initial static 5x5 layout rendering from js && clean up body

--- nonograms/js/script.js
+++ nonograms/js/script.js
@@ -15,17 +15,17 @@ const puzzle = [
   [1, 0, 1, 0, 1],
 ];
 
-const gameContainer = document.body;
+let nonogramContainer;
 
-const getRandomQuestion = () => {
-  const randomIndex = Math.floor(Math.random() * questionsData.length);
-  if(randomIndex === oldQuestionIndex) {
-    return getRandomQuestion();
-  } else {
-    oldQuestionIndex = randomIndex;
-    return questionsData[randomIndex];
-  }
-};
+// const getRandomPuzzle = () => {
+//   const randomIndex = Math.floor(Math.random() * questionsData.length);
+//   if(randomIndex === oldQuestionIndex) {
+//     return getRandomPuzzle();
+//   } else {
+//     oldQuestionIndex = randomIndex;
+//     return questionsData[randomIndex];
+//   }
+// };
 
 const renderElement = (child, className, parent, attr) => {
   const element = document.createElement(child);
@@ -39,4 +39,41 @@ const renderElement = (child, className, parent, attr) => {
   return element;
 };
 
-// mainContainer = renderElement("div", "main-container", document.body);
+const initialRender = () => {
+  document.body.innerHTML = "";
+  const mainContainer = renderElement("div", "main-container", document.body);
+  nonogramContainer = renderElement("div", "nonogram-container", mainContainer);
+  fieldRender(puzzle);
+}
+
+const fieldRender = (puzzle) => {
+  puzzle.forEach((arr, rowIndex) => {
+    //top row clues render
+    if(rowIndex === 0) {
+      const rowClues = renderElement("div", "row", nonogramContainer);
+      renderElement("div", "clue --empty", rowClues);
+
+      let rowLength = arr.length;
+      while(rowLength){
+        const topClueElement = renderElement("div", "clue --top", rowClues);
+        topClueElement.innerHTML = 10;
+        rowLength--;
+      }
+    }
+    //row cells render
+    const row = renderElement("div", "row", nonogramContainer);
+
+    const leftClueElement = renderElement("div", "clue --left", row);
+    leftClueElement.innerHTML = 10;
+
+    arr.forEach((el, i) => {
+        if(el === 1){
+          const element = renderElement("div", "cell filled", row);
+        } else {
+          const element = renderElement("div", "cell", row);
+        }
+    })
+  })
+}
+
+initialRender();
