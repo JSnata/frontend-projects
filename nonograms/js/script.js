8a3b20dc04509b187a77c156a35367ab85de5d49
natagetup <n.kulikouskaya@gmail.com>
2024-01-22 19:38:56 +0100

    feat: add basic css/html layout && element renderng function

---
+++ nonograms/js/script.js
@@ -0,0 +1,42 @@
+const initialPuzzle = [
+  [1, 0, 1, 0, 1],
+  [0, 1, 0, 1, 0],
+  [1, 0, 1, 0, 1],
+  [0, 1, 0, 1, 0],
+  [1, 0, 1, 0, 1],
+];
+
+
+const puzzle = [
+  [1, 0, 1, 0, 1],
+  [0, 1, 0, 1, 0],
+  [1, 0, 1, 0, 1],
+  [0, 1, 0, 1, 0],
+  [1, 0, 1, 0, 1],
+];
+
+const gameContainer = document.body;
+
+const getRandomQuestion = () => {
+  const randomIndex = Math.floor(Math.random() * questionsData.length);
+  if(randomIndex === oldQuestionIndex) {
+    return getRandomQuestion();
+  } else {
+    oldQuestionIndex = randomIndex;
+    return questionsData[randomIndex];
+  }
+};
+
+const renderElement = (child, className, parent, attr) => {
+  const element = document.createElement(child);
+  className && (element.className = className);
+  parent && parent.append(element);
+  if (attr) {
+    for (let key in attr) {
+      element[key] = attr[key];
+    }
+  }
+  return element;
+};
+
+// mainContainer = renderElement("div", "main-container", document.body);
