6683c84ed9835c4e52170e8f5b7718646eb28417
natagetup <n.kulikouskaya@gmail.com>
2024-01-23 00:17:54 +0100

    feat: implement clues rendering

--- nonograms/js/script.js
+++ nonograms/js/script.js
@@ -8,11 +8,11 @@ const initialPuzzle = [
 
 
 const puzzle = [
-  [1, 0, 1, 0, 1],
-  [0, 1, 0, 1, 0],
-  [1, 0, 1, 0, 1],
-  [0, 1, 0, 1, 0],
-  [1, 0, 1, 0, 1],
+  [0, 0, 1, 1, 1],
+  [1, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1],
+  [1, 0, 0, 1, 0],
+  [0, 0, 1, 0, 1],
 ];
 
 let nonogramContainer;
@@ -47,33 +47,63 @@ const initialRender = () => {
 }
 
 const fieldRender = (puzzle) => {
-  puzzle.forEach((arr, rowIndex) => {
+  let topClueCounter = 0;
+  puzzle.forEach((rowArr, rowIndex) => {
     //top row clues render
+    let rowClues;
+    let topClueElement;
     if(rowIndex === 0) {
-      const rowClues = renderElement("div", "row", nonogramContainer);
+      rowClues = renderElement("div", "row", nonogramContainer);
       renderElement("div", "clue --empty", rowClues);
-
-      let rowLength = arr.length;
-      while(rowLength){
-        const topClueElement = renderElement("div", "clue --top", rowClues);
-        topClueElement.innerHTML = 10;
-        rowLength--;
-      }
     }
+
     //row cells render
     const row = renderElement("div", "row", nonogramContainer);
 
+    //left clue render
     const leftClueElement = renderElement("div", "clue --left", row);
-    leftClueElement.innerHTML = 10;
-
-    arr.forEach((el, i) => {
+    let leftClueCounter = 0;
+    rowArr.forEach((el, i) => {
+        if(rowIndex === 0){
+          //top clue counters rendering
+          topClueElement = renderElement("div", "clue --top", rowClues);
+          topCluesNumbersArray = getTopClue(puzzle, i);
+          topCluesNumbersArray.forEach((num) => {
+            const clueNumberElement = renderElement("span", "clue-number", topClueElement);
+            clueNumberElement.innerHTML = num;
+          })
+        }
         if(el === 1){
           const element = renderElement("div", "cell filled", row);
+          leftClueCounter += 1;
         } else {
+          leftClueCounter = 0;
           const element = renderElement("div", "cell", row);
+        }
+
+        //fill left clue with counter
+        if((puzzle[rowIndex][i + 1] === 0 || i + 1 >= rowArr.length) && leftClueCounter) {
+          const leftClueNumberElement = renderElement("span", "clue-number", leftClueElement);
+          leftClueNumberElement.innerHTML = leftClueCounter;
         }
     })
   })
+}
+
+getTopClue = (puzzle, elIndex) => {
+  let counter = puzzle[0][elIndex];
+  let rowIndex = 0;
+  const topClues = [];
+  while (rowIndex + 1 <= puzzle.length) {
+    if(puzzle[rowIndex + 1] && puzzle[rowIndex + 1][elIndex]){
+      counter += 1;
+    } else {
+      counter && topClues.push(counter);
+      counter = 0;
+    }
+    rowIndex += 1;
+  }
+  return topClues;
 }
 
 initialRender();
