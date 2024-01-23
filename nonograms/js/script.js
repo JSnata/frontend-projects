3af2457de78ec71a551661476682e79b5d55acec
natagetup <n.kulikouskaya@gmail.com>
2024-01-23 13:29:10 +0100

    feat: implement equality check between user answer and game initial puzzle

--- nonograms/js/script.js
+++ nonograms/js/script.js
@@ -1,20 +1,41 @@
-const initialPuzzle = [
+const puzzleEmpty = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+
+const userPuzzle = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+];
+
+const puzzleSnowFlake = [
   [1, 0, 1, 0, 1],
-  [0, 1, 0, 1, 0],
-  [1, 0, 1, 0, 1],
-  [0, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0],
+  [1, 1, 0, 1, 1],
+  [0, 1, 1, 1, 0],
   [1, 0, 1, 0, 1],
 ];
 
-const puzzle = [
-  [0, 0, 1, 1, 1],
-  [1, 0, 0, 0, 0],
-  [0, 1, 1, 1, 1],
-  [1, 0, 0, 1, 0],
-  [0, 0, 1, 0, 1],
+const puzzleTower = [
+  [1, 0, 1, 0, 1],
+  [1, 1, 1, 1, 1],
+  [0, 1, 1, 1, 0],
+  [0, 1, 0, 1, 0],
+  [0, 1, 1, 1, 0],
 ];
+
+function areArraysEqual(arr1, arr2) {
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
+}
 
 let nonogramContainer;
+let currentPuzzle = puzzleTower;
 
 // const getRandomPuzzle = () => {
 //   const randomIndex = Math.floor(Math.random() * questionsData.length);
@@ -42,7 +63,7 @@ const initialRender = () => {
   document.body.innerHTML = "";
   const mainContainer = renderElement("div", "main-container", document.body);
   nonogramContainer = renderElement("div", "nonogram-container", mainContainer);
-  fieldRender(puzzle, "initial");
+  fieldRender(puzzleTower, "initial");
 };
 
 const fieldRender = (puzzle, fieldMode) => {
@@ -58,23 +79,20 @@ const fieldRender = (puzzle, fieldMode) => {
     //left clue render
     const leftClueElement = renderElement("div", "clue --left", row);
     let leftClueCounter = 0;
-    rowArr.forEach((el, i) => {
-    
-    //cells rendering
 
+    rowArr.forEach((el, i) => {
+    //cells rendering
     let ceilElement;
       if (el === 1) {
-        ceilElement = fieldMode !== "initial" ? renderElement("div", "cell filled", row) : renderElement("div", "cell", row);
+        ceilElement = (fieldMode !== "initial") ? renderElement("div", "cell filled", row) : renderElement("div", "cell", row);
         leftClueCounter += 1;
       } else {
         leftClueCounter = 0;
         ceilElement = renderElement("div", "cell", row);
       }
-
-      ceilElement.addEventListener("click", (e) => {
-        e.target.classList.toggle("filled");
-      })
-
+      ceilElement.setAttribute('data-row', `${rowIndex}`);
+      ceilElement.setAttribute('data-cell', `${i}`);
+      ceilElement.addEventListener("click", (e) => cellClickHandler(e))
       //fill left clue with counter
       if (
         (puzzle[rowIndex][i + 1] === 0 || i + 1 >= rowArr.length) &&
@@ -121,5 +139,22 @@ const getTopClue = (puzzle) => {
     });
   });
 };
+
+cellClickHandler = (e) => {
+  e.target.classList.toggle("filled");
+
+  //made userMatrix;
+  const row = e.target.dataset.row;
+  const cell = e.target.dataset.cell;
+  userPuzzle[row][cell] = e.target.classList.contains("filled") ?  1 : 0;
+  console.log(userPuzzle);
+
+  if (areArraysEqual(userPuzzle, currentPuzzle)) {
+    console.log("Equal");
+  } else {
+    console.log("Not Equal");
+  }
+
+}
 
 initialRender();
